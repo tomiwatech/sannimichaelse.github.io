d92bbc342bddcda402d44c1fe1b97f2ce5a7ab62
Sanni Michael Tomiwa <tomiwatech@gmail.com>
2018-07-15 06:44:50 +0100

    Modified portfolio

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@
 	/*--/ Star Typed /--*/
 	if ($('.text-slider').length == 1) {
 		var typed = new Typed('.text-slider', {
-			strings: ["Serial Entrepreneur","JavaScript Artisan.", "Python Advocate.", "Ionic Lover.", " Data Structures and Algorithms Freak"],
+			strings: ["Serial Entrepreneur","JavaScript Artisan.","Python Advocate.","AI Enthusiast","Data Structures and Algorithms Freak"],
 			typeSpeed: 80,
 			loop: true,
 			backDelay: 1100,
